570c812b59a45f2ca8f2af504819e0e5921a84f6
anna <annapecheanu1@gmail.com>
2024-04-03 17:39:13 +0300

    Setup Jest for frontend testing

---
+++ frontend/__tests__/App.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react-native';
+import App from '../src/App';
+
+describe('App', () => {
+    it('should display text', () => {
+        const { getByText } = render(<App />);
+        expect(getByText('Open up App.tsx to start working on your app!')).not.toBeNull();
+    });
+});
