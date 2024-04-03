4c3773f4a4036a51327429a57468c639b83fc32d
anna <annapecheanu1@gmail.com>
2024-04-03 16:08:04 +0300

    Made eslint configuration global

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,35 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {useEffect, useState} from "react";
-import axios from "axios";
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-export default function App() {
-  const [myData, setMyData] = useState('');
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://192.168.191.158:3000/api');
-        setMyData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        }
-    };
-    fetchData();
-  }, []);
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>response {myData}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default function App(): JSX.Element {
+    const [myData, setMyData] = useState<string>('');
+    useEffect(() => {
+        const fetchData = async (): Promise<void> => {
+            try {
+                const response = await axios.get<string>('http://192.168.191.158:3000/api');
+                setMyData(response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+    }, []);
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <Text>response {myData}</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
