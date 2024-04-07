78d969b3acaadf66187284275c8aaff1c93ba17d
anna <annapecheanu1@gmail.com>
2024-04-07 14:21:22 +0300

    Fixed eslint for backend as well

--- backend/src/configure.ts
+++ backend/src/configure.ts
@@ -3,15 +3,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const getEnvironmentVariable = (key: string): string => {
-    const value = process.env[key];
-    if (!value) {
-        throw new Error(`Environment variable ${key} not set`);
-    }
-    return value;
+	const value = process.env[key];
+	if (!value) {
+		throw new Error(`Environment variable ${key} not set`);
+	}
+	return value;
 };
 
 export const config = {
-    mongo_uri: getEnvironmentVariable('MONGO_URI'),
-    port: parseInt(getEnvironmentVariable('PORT')),
-    clerk_secret_key: getEnvironmentVariable('CLERK_SECRET_KEY')
+	mongo_uri: getEnvironmentVariable('MONGO_URI'),
+	port: parseInt(getEnvironmentVariable('PORT')),
+	clerk_secret_key: getEnvironmentVariable('CLERK_SECRET_KEY')
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,35 +7,35 @@ export const app = express();
 const port = config.port;
 
 app.use((req, res, next) => {
-    const now = new Date().toISOString();
-    console.log(`${now} - ${req.method} ${req.path} - IP: ${req.ip}`);
-    next();
+	const now = new Date().toISOString();
+	console.log(`${now} - ${req.method} ${req.path} - IP: ${req.ip}`);
+	next();
 });
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	next();
 });
 
 app.get('/api', (req, res) => {
-    res.send('Hello World!');
+	res.send('Hello World!');
 });
 
 app.get('/api/secret', ClerkExpressWithAuth(), (req, res) => {
-    res.send('Secret data');
+	res.send('Secret data');
 });
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Server is running at http://localhost:${port}`);
+	console.log(`Server is running at http://localhost:${port}`);
 });
 
 connect().then(() => {
-    console.log('Database connected');
-    seedDatabase().then(() => {
-        console.log('Database seeded');
-    }).catch((error) => {
-        console.error('Database seeding failed');
-    });
+	console.log('Database connected');
+	seedDatabase().then(() => {
+		console.log('Database seeded');
+	}).catch((error) => {
+		console.error('Database seeding failed');
+	});
 });
