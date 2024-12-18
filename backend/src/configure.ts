506f35bd05dc4130b6f2da56703be51d77b99478
Anna Pecheanu <annapecheanu1@gmail.com>
2024-12-18 13:21:15 +0200

    secured shared database

--- backend/src/configure.ts
+++ backend/src/configure.ts
@@ -12,6 +12,9 @@ const getEnvironmentVariable = (key: string): string => {
 
 export const config = {
 	mongo_uri: getEnvironmentVariable('MONGO_URI'),
+	mongo_user: getEnvironmentVariable('MONGO_USER'),
+	mongo_pass: getEnvironmentVariable('MONGO_PASS'),
+	mongo_db_name: getEnvironmentVariable('MONGO_DB_NAME'),
 	port: parseInt(getEnvironmentVariable('PORT')),
 	clerk_secret_key: getEnvironmentVariable('CLERK_SECRET_KEY')
 };
--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,9 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose, {mongo} from 'mongoose';
 import { config } from '../configure';
 
 const connect = async (): Promise<void> => {
+	const { mongo_uri, mongo_user, mongo_pass, mongo_db_name } = config;
 	try {
-		await mongoose.connect(config.mongo_uri);
+		await mongoose.connect(mongo_uri, {
+			user: mongo_user,
+			pass: mongo_pass,
+			dbName: mongo_db_name,
+			authSource: mongo_db_name
+		});
 		console.log('Database connected');
 	} catch (error) {
 		console.error('Database connection failed');
