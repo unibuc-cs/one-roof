f0e41b005f687684af82f3aa282ce8eaedd0a519
Anastasia Stefanescu <anastasia@stefanescu.eu>
2025-01-27 23:05:29 +0200

    tried to fix dependencies errors caused by wrongly installed testing libraries

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,20 +1,14 @@
 module.exports = function (api) {
 	api.cache(true);
 	return {
-		presets: ['babel-preset-expo',
-			['@babel/preset-env', {targets: {node: 'current'}}],
-			'@babel/preset-react',
-			'@babel/preset-typescript'
-		],
+		presets: ['babel-preset-expo'],
 		plugins: [
 			['module:react-native-dotenv', {
 				moduleName: '@env',
 				path: '.env',
 				safe: true,
 				allowUndefined: false
-			}],
-			'babel-plugin-transform-es2015-modules-commonjs',
-			'react-native-reanimated/plugin',
+			}]
 		]
 	};
 };
